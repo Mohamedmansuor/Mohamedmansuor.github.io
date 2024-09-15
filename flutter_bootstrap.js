976f24349bc9bfd39e2e7a780b0657a94f6d1909
Mohammed Mansour <mohammad.mansuor3@gmail.com>
2024-09-15 04:56:07 +0300

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2030610593"
+    serviceWorkerVersion: "3353583540"
   }
 });
