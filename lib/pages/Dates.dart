163679313e08f258a933250e77afc88bd11ef10b
Mohammed Mansour <mohammad.mansuor3@gmail.com>
2024-10-06 09:15:09 +0300

    editing assets

--- lib/pages/Dates.dart
+++ lib/pages/Dates.dart
@@ -5,37 +5,37 @@ import 'package:jess/model/Dates_Model.dart';
 class Dates extends StatelessWidget {
   final List<DatesModel> cards = [
     DatesModel(
-      imagePath: "images/jess/photo1.webp",
+      imagePath: "assets/images/jess/photo1.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo1.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo3.webp",
+      imagePath: "assets/images/jess/photo3.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo2.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo4.webp",
+      imagePath: "assets/images/jess/photo4.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo3.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo5.webp",
+      imagePath: "assets/images/jess/photo5.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo4.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo6.webp",
+      imagePath: "assets/images/jess/photo6.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo5.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo7.webp",
+      imagePath: "assets/images/jess/photo7.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo6.wav",
     ),
     DatesModel(
-      imagePath: "images/jess/photo8.webp",
+      imagePath: "assets/images/jess/photo8.webp",
       sentence: 'How this day feels:',
       audioPath: "sounds/photo7.wav",
     ),
